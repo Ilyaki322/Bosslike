2f42108618a9cd68129f9701ee7e2f2841ae7975
Izumme <ilyakirshtein@gmail.com>
2025-04-18 01:41:41 +0300

    Added rotation (still buggy multiplayer) Added networkButtons (cause error on close for some reason?)

--- Assets/Scripts/PlayerScripts/PlayerMovement_Network.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement_Network.cs
@@ -7,16 +7,13 @@ public class PlayerMovement_Network : NetworkBehaviour {
 
     [SerializeField] private Rigidbody2D m_rb;
     [SerializeField] private float m_speed = 5f;
+    [SerializeField] private Transform m_player;
+
     private Vector2 m_moveUpdate;
 
-    public override void OnNetworkSpawn() {
-        
-    }
-
     private void FixedUpdate() {
-        if (!IsOwner) return;
-
         ProcessMovement();
+        LookAtMouse();
     }
 
     private void ProcessMovement()
@@ -25,6 +22,22 @@ public class PlayerMovement_Network : NetworkBehaviour {
             m_rb.linearVelocity = m_moveUpdate.normalized * m_speed;
         }
         else m_rb.linearVelocity = Vector2.zero;
+
+        Debug.DrawLine(transform.position, transform.position + new Vector3(m_rb.linearVelocity.x, m_rb.linearVelocity.y, 0));
+    }
+
+    private void LookAtMouse()
+    {
+        Vector3 mouseScreenPos = Input.mousePosition;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+
+        Vector2 direction = mouseWorldPos - m_player.position;
+
+        if (direction.magnitude > 0.1f)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            m_player.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
     }
 
     public void OnMove(InputValue context)
